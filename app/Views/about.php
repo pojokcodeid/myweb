88e385259c1d54a4404f4ca67bdf37dc4f9cf19a
asep komarudin <pojokcodeid@gmail.com>
2023-07-09 18:15:07 +0700

    update

--- app/Views/about.php
+++ app/Views/about.php
@@ -5,6 +5,34 @@
   <div class="row">
     <div class="col">
       <h1>Pojok Code </h1>
+      <h3>Apa itu Pojok Code ?</h3>
+      <p>Pojok Code aadalah sebuah website yang menyediakan, materi seputar teknik informatika dan bahasa pemrograman.
+        anda bisa mempelajarinya secara gratis dan dapat melakukan diskusi pada hapaman forum
+      </p>
+      <p>Jika anda ingin mendapatkan fasilitas dan pelayanan premium maka anda dapat mengikuti program member, silahkan
+        lakukan registrasi pada tautan berikut</p>
+      <h3>Siapa Owner dari Pojok Code ?</h3>
+      <table cellpadding="5" cellspacing="0">
+        <tr>
+          <td>Nama</td>
+          <td>:</td>
+          <td>Asep komarudin</td>
+        </tr>
+        <tr>
+          <td>Alamat</td>
+          <td>:</td>
+          <td>Depok Jawa Barat</td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td>:</td>
+          <td>pojokcodeid@gmail.com</td>
+        </tr>
+      </table>
+      <div class="text-end mt-5">
+        <p>Hormat Kami</p>
+        <p>Pojok Code</p>
+      </div>
     </div>
   </div>
 </div>
--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -6,7 +6,8 @@
         <div class="col-6 col-md-2 mb-3">
           <h5>Perusahaan</h5>
           <ul class="nav flex-column">
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Tentang Kami</a></li>
+            <li class="nav-item mb-2"><a href="http://localhost/blog/about"
+                class="nav-link p-0 text-body-secondary">Tentang Kami</a></li>
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Hubungi Kami</a></li>
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Kebijakan Privasi</a></li>
           </ul>
