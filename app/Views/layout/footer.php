1b9f8df7bf73c0e2ff36e3847c36375c30fdff21
asep komarudin <pojokcodeid@gmail.com>
2023-07-09 18:00:32 +0700

    update

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -13,18 +13,22 @@
         </div>
 
         <div class="col-6 col-md-2 mb-3">
-          <h5>Layanan</h5>
+          <h5>Media Sosial</h5>
           <ul class="nav flex-column">
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Tutorial</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Arikel</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Forum</a></li>
+            <li class="nav-item mb-2"><a href="https://www.facebook.com/pojokkode" target="_blank"
+                class="nav-link p-0 text-body-secondary"><i class="bi bi-facebook"></i> Facebook</a></li>
+            <li class="nav-item mb-2"><a href="https://www.instagram.com/pojokcodeid_/" target="_blank"
+                class="nav-link p-0 text-body-secondary"><i class="bi bi-instagram"></i> Instagram</a></li>
+            <li class="nav-item mb-2"><a href="https://twitter.com/pojokcodeid_" target="_blank"
+                class="nav-link p-0 text-body-secondary"><i class="bi bi-twitter"></i> Twitter</a></li>
+            <li class="nav-item mb-2"><a href="https://www.tiktok.com/@pojokcodeid" target="_blank"
+                class="nav-link p-0 text-body-secondary"><i class="bi bi-tiktok"></i> Tiktok</a></li>
           </ul>
         </div>
 
         <div class="col-md-5 offset-md-1 mb-3">
           <form>
             <h5>Langganan</h5>
-            <!-- <p>Monthly digest of what's new and exciting from us.</p> -->
             <div class="d-flex flex-column flex-sm-row w-100 gap-2">
               <label for="newsletter1" class="visually-hidden">Email address</label>
               <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
@@ -38,17 +42,6 @@
         <p>&copy;
           <?= date("Y") ?> pojokcode. All rights reserved.
         </p>
-        <ul class="list-unstyled d-flex">
-          <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
-                <use xlink:href="#twitter" />
-              </svg></a></li>
-          <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
-                <use xlink:href="#instagram" />
-              </svg></a></li>
-          <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
-                <use xlink:href="#facebook" />
-              </svg></a></li>
-        </ul>
       </div>
     </div>
   </footer>
